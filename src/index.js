2a7e36fca076b132be2c4950dbdb11285b01bd0f
katydecorah <katydecorah@gmail.com>
2021-09-26 11:20:42 -0400

    Revert test case

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 "use strict";
 
 const core = require("@actions/core");
-// const github = require("@actions/github");
+const github = require("@actions/github");
 const isbn = require("node-isbn");
 const { titleParser, getBook, writeFile } = require("./utils");
 
 async function read() {
   try {
-    const title = "9780525620792";
-    const number = 9;
-    const body = "";
+    const { title, number, body } = github.context.payload.issue;
     const { bookIsbn, date } = titleParser(title);
     const fileName = core.getInput("readFileName");
     const providers = core.getInput("providers")
