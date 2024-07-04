64c1f97940d13e99034e6853cf9b7b6b85c31b0b
Katy DeCorah <katydecorah@gmail.com>
2024-07-03 20:18:14 -0400

    open-graph-scraper@6.6.2 (#177)
    * open-graph-scraper@6.6.2
    
    * Build action
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/providers/libby.ts
+++ src/providers/libby.ts
@@ -1,7 +1,7 @@
 import ogs from "open-graph-scraper";
 import { BookParams } from "..";
 import * as cheerio from "cheerio";
-import { OgObject } from "open-graph-scraper/dist/lib/types";
+import { OgObject } from "open-graph-scraper/types/lib/types";
 import { formatDescription, getLibbyId } from "../utils";
 import { NewBook } from "../new-book";
 
